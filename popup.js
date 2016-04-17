cac029aba2c9abff9e5bc7a74e4c5e5c1f4a6a3d
jamesw8 <jameswong8@gmail.com>
2016-04-17 05:40:30 -0400

    suh

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 function addObj() {
-  var obj = document.getElementById("newObjective").value;
-  var ded = document.getElementById("newDeadline").value;
-  if(obj == "" || ded == "") {
-    alert("Objective text field is empty");
+   var obj = document.getElementById("newObjective").value;
+   var ded = document.getElementById("newDeadline").value;
+   if(obj == "" || ded == "") {
+     alert("Objective text field is empty");
   } else {
     alert("obj is "+obj+" and ded is "+ded);
     alert(document.getElementById("lifegiver").outerHTML);
