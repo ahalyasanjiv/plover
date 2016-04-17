e042948f31a5d4d29a0b913de8d723c54fc088d0
jamesw8 <jameswong8@gmail.com>
2016-04-17 04:44:29 -0400

    vanilla

--- popup.js
+++ popup.js
@@ -1,14 +1,13 @@
-
-jQuery('button.newGoal').click(function(event) {
-  alert("HI");
-  event.preventDefault();
+function addObj() {
   var obj = document.getElementById("newObjective").value;
   var ded = document.getElementById("newDeadline").value;
   if(obj == "" || ded == "") {
     alert("Objective text field is empty");
   } else {
-    var newRow = jQuery('<tr><td>'+obj+'</td><td>'+ded+'</td></tr>');
-    jQuery('table.objectTable').append(newRow);
-    console.log("carried out");
+    alert("obj is "+obj+" and ded is "+ded);
+    alert(document.getElementById("lifegiver").outerHTML);
+    d=document.getElementById("lifegiver");
+    var stringerino = "<tr><td>"+obj+"</td>\t\t<td>"+ded+"</td></tr><br><br id='lifegiver'>";
+    d.outerHTML = stringerino;
   }
-});
+}
